16cbc8125c03580f105dc9b9d0c2d9036ddedacf
Breno Barbaro <43378543+BrenoBarbaro@users.noreply.github.com>
2019-03-01 21:57:50 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Teste t1 = new Teste("t1",80);
+            Teste t2 = new Teste("t2",40);
+            Teste t3 = new Teste("t3",20);
+
+            t1.ImprimeLinha();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Teste.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public class Teste
+    {
+        public int tamanho;
+        public Teste(String nome, int tamanho)
+        {
+            Console.WriteLine("O construtor {0} esta rodando...", nome);
+            this.tamanho = tamanho;
+        }
+
+        public void ImprimeLinha()
+        {
+            Console.WriteLine("");
+            for(int x = 0; x < tamanho; x++)
+            {
+                Console.Write("-");
+            }
+            Console.WriteLine("");
+        }
+    }
+}
